9eb57d62c85ae064d701253de6aea3e1e5d7c684
John Marks <johnjmarks4@gmail.com>
2017-09-28 01:10:26 -0500

    Second attempt, faster

--- length_of_longest_substring.rb
+++ length_of_longest_substring.rb
@@ -1,23 +1,36 @@
-# @param {String} s
-# @return {Integer}
 def length_of_longest_substring(s)
+  return 0 if s == ""
   ary = s.split('')
   subs = []
-  ary.each_with_index do |e, i|
-    ary[i+1..-1].each_with_index do |f, j|
-      j += i + 1
-      if s[i..j].split('').uniq.length == s[i..j].split('').length
-        subs << s[i..j]
+  dups = []
+  i = 0
+  x = 0
+  ary.each do |e|
+    next if dups.include?(e)
+    if ary.count(e) > 1
+      dups << e
+      x += 1
+      until dups.include?(ary[i+=1]) || i > ary.length - 1
+        dups << e if ary.count(ary[i]) > 1
+        x += 1
       end
+      subs << x
+      x = 0
+    else
+      x += 1
+      i += 1
     end
   end
-  return 0 if ary[0].nil?
-  subs << ary[0] if subs.empty?
-  subs.max_by(&:length).length
+  subs << x if x != 0
+  subs = [1] if subs.empty?
+  subs.max
 end
 
-alph = ("a".."z").to_a
+print length_of_longest_substring("abba")
 
+#alph = ("a".."z").to_a
+
+=begin
 900.times do
   s = ""
   rand(1..50).times do
@@ -25,3 +38,4 @@ alph = ("a".."z").to_a
   end
   puts length_of_longest_substring(s)
 end
+=end
