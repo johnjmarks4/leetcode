81aacb1fd1efb47eb3358d36fd057a6c19b3dfac
John Marks <johnjmarks4@gmail.com>
2017-10-11 20:58:09 -0500

    Finish bruteforce algorithm

--- longest_palsub.rb
+++ longest_palsub.rb
@@ -1,54 +1,30 @@
 public
 
-def substring(s)
+# optimize with dynamic programming 
+
+def longest_palindrome(s)
   return "" if s == ""
-  return s if s.size == 1
-  solutions = []
-  temp = []
-  subs = []
   ary = s.split('')
-  ary.each_with_index do |char, i|
-    temp << char
-    if temp.uniq.length != temp.length
-      subs << temp.slice(temp.index(char), i + 1)
+  subs = []
+  ary.each_with_index do |e, start|
+    if ary.count(e) > 1
+      finish = -1
+      until start + finish.abs >= ary.length
+        if e == ary[finish]
+          subs << ary[start..finish]
+        end
+        finish -= 1
+      end
     end
   end
-  return "" if subs.empty?
-  subs.select! { |s| palindromic?(s) }
-  subs.max_by { |s| s.length }.join
+  subs.select! { |e| palindrome?(e) }
+  longest = subs.max_by(&:length)
+  return s[0] if longest.nil?
+  longest.join
 end
 
-def palindromic?(subs)
-  if subs.length <= 1
-    return subs
-  else
-    palindromic?(subs[1..-1]) == palindromic?(subs.reverse[1..-1])
-  end
+def palindrome?(e)
+  e == e.reverse
 end
 
-def solution_test(char_length, test_size)
-  alpha = ("a".."z").to_a
-  all_pals = []
-  test_size.times do
-    z_num = rand(1..char_length - 2)
-    sub_size = char_length - z_num
-    pal1 = make_pal(sub_size, alpha)
-    pal2 = make_pal(sub_size, alpha)
-    all_pals << (pal1 + "z" + pal2)
-  end
-  all_pals.each do |s|
-    puts substring(s)
-  end
-end
-
-def make_pal(sub_size, alpha)
-  string = ""
-  string = ""
-  sub_size.times do
-    letter = alpha[rand(0..25)]
-    string << letter
-  end
-  string + string.reverse
-end
-
-solution_test(6, 4)
+print longest_palindrome("bb")
