cc4a6f143a78b7b372d1d55d9d0be492f40e03b2
John Marks <johnjmarks4@gmail.com>
2017-10-13 17:23:07 -0500

    Initial commit

---
+++ fib_dp.rb
@@ -0,0 +1,25 @@
+# Finds the fibonacci sequence up to n numbers using dynamic programming
+
+def fib(n, memo)
+  if n <= 0
+    return n
+  elsif n <= 1
+    return n
+  elsif n <= memo.length
+    return [memo[n-1], memo]
+  else
+    temp_fib = (fib(n-1, memo)[0] + fib(n-2, memo)[0])
+    memo << temp_fib
+    [temp_fib, memo]
+  end
+end
+
+memo = [1]
+
+loop do
+  number = gets.chomp!.to_i
+  result = fib(number, memo)
+  memo = result[1] if result[1] != 0
+  puts "The fibonacci sequence up to #{number} numbers:"
+  puts memo[0..number-1].inspect
+end
